10815b48ccbeaf1b85912864d0577184fb45084d
traviscomber <n3uralia@gmail.com>
2026-08-04 16:23:06 -0400

    Expose pre-arrival preparation from bookings

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -2,11 +2,13 @@
 
 import BookingOperationsTimelinePage from "./operations/page"
 import { BookingArrivalQueue } from "@/components/booking-arrival-queue"
+import { BookingPrearrivalControl } from "@/components/booking-prearrival-control"
 
 export default function BookingsPage() {
   return (
     <>
       <BookingOperationsTimelinePage />
+      <BookingPrearrivalControl />
       <BookingArrivalQueue />
     </>
   )
