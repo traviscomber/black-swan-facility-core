c45de5120c627929b4eeb8cfefa2dece3c8e2740
traviscomber <n3uralia@gmail.com>
2026-08-04 13:28:42 -0400

    Expose room readiness from booking view

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -1,1 +1,13 @@
-export { default } from "./operations/page"
+"use client"
+
+import BookingOperationsTimelinePage from "./operations/page"
+import { BookingArrivalQueue } from "@/components/booking-arrival-queue"
+
+export default function BookingsPage() {
+  return (
+    <>
+      <BookingOperationsTimelinePage />
+      <BookingArrivalQueue />
+    </>
+  )
+}
