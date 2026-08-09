4ee7b537b795083cb83a54794e29784d0b93d00f
traviscomber <n3uralia@gmail.com>
2026-08-09 13:29:52 -0400

    feat(hospitality): add internal guest requests page

---
+++ app/bookings/requests/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { AppLayout } from "@/components/app-layout"
+import { GuestRequestsInbox } from "@/components/guest-requests-inbox"
+
+export default function GuestRequestsOperationsPage() {
+  return (
+    <AppLayout>
+      <GuestRequestsInbox />
+    </AppLayout>
+  )
+}
