1c756f54afe9d78c2134ad8de52aa1763992e6ee
traviscomber <n3uralia@gmail.com>
2026-08-14 11:17:42 -0400

    Add events workspace page

---
+++ app/os/events/page.tsx
@@ -0,0 +1,5 @@
+import { OsWorkspace } from '@/components/os-workspace'
+
+export default function Page() {
+  return <OsWorkspace workspace="events" title="Events" description="Member-linked event planning, external service providers, operational status and Education output." />
+}
