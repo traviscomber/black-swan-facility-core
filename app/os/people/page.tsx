4b9b90d38c35056fa1b1f4eb67a938fd22da8478
traviscomber <n3uralia@gmail.com>
2026-08-14 11:17:26 -0400

    Add People Graph workspace route

---
+++ app/os/people/page.tsx
@@ -0,0 +1,5 @@
+import { OsWorkspace } from '@/components/os-workspace'
+
+export default function Page() {
+  return <OsWorkspace workspace="people" title="People Graph" description="Members are the root people object. Guests remain attached to the inviting Member, with current on-ground presence controlling guest access." />
+}
