2a1e85b35a78a07f9f40443866f9f1d961e84918
traviscomber <n3uralia@gmail.com>
2026-08-15 17:05:45 -0400

    Expose TUU provider capabilities

--- cloudflare/workers/banking/src/providers/index.ts
+++ cloudflare/workers/banking/src/providers/index.ts
@@ -3,12 +3,14 @@ import { fintocAdapter } from './fintoc'
 import { khipuAdapter } from './khipu'
 import { transbankAdapter } from './transbank'
 import { stripeAdapter } from './stripe'
+import { tuuAdapter } from './tuu'
 
 const adapters: Record<ProviderKey, FinancialProviderAdapter> = {
   fintoc: fintocAdapter,
   khipu: khipuAdapter,
   transbank: transbankAdapter,
   stripe: stripeAdapter,
+  tuu: tuuAdapter,
 }
 
 export function getProviderAdapter(key: string) {
