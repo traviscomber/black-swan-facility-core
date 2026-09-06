fb55b6ec754524c6ef5b5568ccc941583c2dff65
traviscomber <n3uralia@gmail.com>
2026-09-06 12:57:37 -0300

    Add Orchard task management settings route

---
+++ app/orchard/settings/task-management/page.tsx
@@ -0,0 +1,5 @@
+import { OrchardSettingsSection } from "@/components/orchard/orchard-settings-section"
+
+export default function OrchardTaskManagementPage(){
+  return <OrchardSettingsSection mode="task-management"/>
+}
