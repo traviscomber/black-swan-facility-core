5e3418f28b1839607a6dded450186364bfa34a54
traviscomber <n3uralia@gmail.com>
2026-09-06 12:57:31 -0300

    Add Orchard sales channels settings route

---
+++ app/orchard/settings/sales-channels/page.tsx
@@ -0,0 +1,5 @@
+import { OrchardSettingsSection } from "@/components/orchard/orchard-settings-section"
+
+export default function OrchardSalesChannelsPage(){
+  return <OrchardSettingsSection mode="sales-channels"/>
+}
