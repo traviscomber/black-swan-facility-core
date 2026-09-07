b35378e56fb44996eadfdef269379f75221aa154
traviscomber <n3uralia@gmail.com>
2026-09-07 12:17:46 -0300

    refactor(orchard): remove farm map mode toggle

--- app/orchard/farm-map/layout.tsx
+++ app/orchard/farm-map/layout.tsx
@@ -1,11 +1,7 @@
 import type { ReactNode } from "react"
 import "leaflet/dist/leaflet.css"
 import "./farm-map-calm.css"
-import { FarmMapModeNav } from "./farm-map-mode-nav"
 
 export default function FarmMapLayout({ children }: { children: ReactNode }) {
-  return <div data-orchard-farm-map-layout className="relative">
-    {children}
-    <FarmMapModeNav/>
-  </div>
+  return <div data-orchard-farm-map-layout className="relative">{children}</div>
 }
