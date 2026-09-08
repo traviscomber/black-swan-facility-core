898ae189226adc450068b05067cdadb49d39cb76
traviscomber <n3uralia@gmail.com>
2026-09-08 17:00:43 -0300

    security(db): revoke anon external Orchard helpers

---
+++ supabase/migrations/20260908200030_harden_external_orchard_helper_acl.sql
@@ -0,0 +1,8 @@
+revoke execute on function public.is_external_orchard_user() from anon;
+revoke execute on function public.can_access_external_orchard_location(uuid) from anon;
+revoke execute on function public.owns_external_orchard_game_plan(uuid) from anon;
+revoke execute on function public.owns_external_orchard_cycle(uuid) from anon;
+revoke execute on function public.owns_external_orchard_succession(uuid) from anon;
+revoke execute on function public.owns_external_orchard_seed_lot(uuid) from anon;
+revoke execute on function public.owns_external_orchard_sales_channel(uuid) from anon;
+revoke execute on function public.owns_external_orchard_demand_scenario(uuid) from anon;
