9e30b20052577442824793a9758fcdb8c5b4a111
traviscomber <n3uralia@gmail.com>
2026-09-08 22:22:30 -0300

    feat(orchard): add visual truth policy for non-canonical imagery

---
+++ components/orchard/orchard-visual-truth-policy.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+const ORCHARD_VISUAL_TRUTH_CSS = `
+[data-orchard-image-policy] img[src*="images.unsplash.com"],
+[data-orchard-image-policy] img[src*="source.unsplash.com"],
+[data-orchard-image-policy] img[src*="unsplash.com/photos"],
+[data-orchard-image-policy] img[src*="images.pexels.com"],
+[data-orchard-image-policy] img[src*="pixabay.com"] {
+  display: none !important;
+}
+
+[data-orchard-image-policy] :is(figure, picture):has(> img[src*="unsplash.com"]),
+[data-orchard-image-policy] :is(figure, picture):has(> img[src*="pexels.com"]),
+[data-orchard-image-policy] :is(figure, picture):has(> img[src*="pixabay.com"]) {
+  display: none !important;
+}
+
+[data-orchard-image-policy] :is(div, section):has(> img[src*="images.unsplash.com"]),
+[data-orchard-image-policy] :is(div, section):has(> img[src*="source.unsplash.com"]),
+[data-orchard-image-policy] :is(div, section):has(> img[src*="unsplash.com/photos"]),
+[data-orchard-image-policy] :is(div, section):has(> img[src*="images.pexels.com"]),
+[data-orchard-image-policy] :is(div, section):has(> img[src*="pixabay.com"]) {
+  background-color: hsl(var(--card)) !important;
+}
+
+[data-orchard-image-policy] :is(div, section):has(> img[src*="images.unsplash.com"]) > [class*="absolute"][class*="inset-0"],
+[data-orchard-image-policy] :is(div, section):has(> img[src*="source.unsplash.com"]) > [class*="absolute"][class*="inset-0"],
+[data-orchard-image-policy] :is(div, section):has(> img[src*="unsplash.com/photos"]) > [class*="absolute"][class*="inset-0"],
+[data-orchard-image-policy] :is(div, section):has(> img[src*="images.pexels.com"]) > [class*="absolute"][class*="inset-0"],
+[data-orchard-image-policy] :is(div, section):has(> img[src*="pixabay.com"]) > [class*="absolute"][class*="inset-0"] {
+  background: transparent !important;
+}
+
+@media (max-width: 767px) {
+  [data-orchard-image-policy] :is(div, section):has(> img[src*="unsplash.com"]),
+  [data-orchard-image-policy] :is(div, section):has(> img[src*="pexels.com"]),
+  [data-orchard-image-policy] :is(div, section):has(> img[src*="pixabay.com"]) {
+    min-height: 0 !important;
+  }
+}
+`
+
+export function OrchardVisualTruthPolicy() {
+  return <style>{ORCHARD_VISUAL_TRUTH_CSS}</style>
+}
