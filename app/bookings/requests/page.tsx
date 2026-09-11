ff72aa9280859c313ac3aa533370e77d8ab93eeb
traviscomber <n3uralia@gmail.com>
2026-09-10 21:21:14 -0300

    booking: keep requests inside booking workspace shell

--- app/bookings/requests/page.tsx
+++ app/bookings/requests/page.tsx
@@ -1,12 +1,7 @@
 "use client"
 
-import { AppLayout } from "@/components/app-layout"
 import { GuestRequestsInbox } from "@/components/guest-requests-inbox"
 
 export default function GuestRequestsOperationsPage() {
-  return (
-    <AppLayout>
-      <GuestRequestsInbox />
-    </AppLayout>
-  )
+  return <GuestRequestsInbox />
 }
